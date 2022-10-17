40710a38097e09f2f23f299ee8b5d3e7826ddbc3
Amber916Young <youngyejia@163.com>
2022-10-17 12:22:13 +0100

    groupingDishes

--- src/HashTable/groupingDishes.java
+++ src/HashTable/groupingDishes.java
@@ -16,41 +16,32 @@ public class groupingDishes {
 //                ,{"Quesadilla", "Chicken", "Cheese", "Sauce"}
 //                ,{"Sandwich", "Salad", "Bread", "Tomato", "Cheese"}
 //    };
-    String[][] groupingDishes(String[][] dishes) {
-        HashMap<String, List<String>> map= new HashMap<>();
-        for(int i=0; i< dishes.length; i++){
-            for(int  j=1; j< dishes[i].length; j++){
-                List<String> list;
-                if(!map.containsKey(dishes[i][j])){
-                    list = new ArrayList<String>();
-
-                }else{
-                    list = map.get(dishes[i][j]);
-                }
-                list.add(dishes[i][0]);
-                map.put(dishes[i][j], list);
-
+    String[][] solution(String[][] dishes) {
+        HashMap<String, List<String>> ingredients_food = new HashMap<>();
+        for(String[] dishe : dishes){
+            String name = dishe[0];
+            for(int i = 1;i<dishe.length;i++){
+                List<String> list = ingredients_food.getOrDefault(dishe[i], new ArrayList<>());
+                list.add(name);
+                ingredients_food.put(dishe[i], list);
             }
         }
-        TreeMap<String, List<String>> treemap = new TreeMap<>(map);
-        List<List<String>> reList= new ArrayList<>();
+        TreeMap<String, List<String>> treemap = new TreeMap<>(ingredients_food);
+        List<List<String>> ansList= new ArrayList<>();
         for(String key: treemap.keySet()){
             if(treemap.get(key).size()>=2){
                 List<String> subList= treemap.get(key);
                 Collections.sort(subList);
                 subList.add(0,key);
-                reList.add(subList);
+                ansList.add(subList);
             }
         }
-
-        System.out.println(reList.toString());
-        String[][] array = new String[reList.size()][];
-        for (int i = 0; i < reList.size(); i++) {
-            List<String> row = reList.get(i);
-            array[i] = row.toArray(new String[row.size()]);
+        String[][] ans = new String[ansList.size()][];
+        for(int i = 0;i < ansList.size() ; i++){
+            List<String> row = ansList.get(i);
+            ans[i] = row.toArray(new String[row.size()]);
         }
-
-        return array;
+        return ans;
     }
 
 
